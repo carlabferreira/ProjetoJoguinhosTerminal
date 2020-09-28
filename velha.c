a0da41daa745bc43addb1e0a83d75d7521e603aa
Carla Ferreira <carlabeatrizferreira04@gmail.com>
2020-09-28 03:58:44 -0300

    inicio

--- velha.c
+++ velha.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 
 int main(){
-    int terminou = 1;
-    char primeirojogador[] = "Victor", segundojogador[] = "kkarla";
+    printf("\n---------------------------------");
+    printf("\n|  BEM-VINDO AO JOGO DA VELHA   |");
+    printf("\n| Escolha sua opcao:            |");
+    printf("\n| (1) 'o' começa                |");
+    printf("\n| (2) 'x' começa                |");
+    printf("\n| (3) Sair                      |");
+    printf("\n              by Carla and Victor");
+    printf("\n---------------------------------");
 
-    printf("Quem começa, ?")
-    printf 
-    while(terminou){
-        printf("");
-        scanf
+    printf("\n\n->");
+    scanf("%i", &opcao);
+    //printf("\nA opcao escolhida foi %i", opcao);
+
+    switch (opcao){
+    case 1:
+        
+
+    default:
+    printf("Opcao invalida, saindo...\n");
+    system("pause");
     }
+
+    return (0);
+}
+
 }
