487e0821921ad7432721174a7313c862e2c74b32
VictorAugustoqwe <vaugustocruz22@gmail.com>
2020-09-28 03:55:57 -0300

    oi

--- velha.c
+++ velha.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
 int main(){
+    int terminou = 1;
+    char primeirojogador[] = "Victor", segundojogador[] = "kkarla";
 
+    printf("Quem começa, ?")
+    printf 
+    while(terminou){
+        printf("");
+        scanf
+    }
 }
