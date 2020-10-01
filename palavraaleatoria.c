dee14b534b4f7b61612bd67129004f0a030c9304
VictorAugustoqwe <vaugustocruz22@gmail.com>
2020-10-01 01:33:25 -0300

    taae

--- palavraaleatoria.c
+++ palavraaleatoria.c
@@ -6,15 +6,21 @@
 
 int main() {
     FILE* arquivo = fopen("palavras.txt", "r");
+    FILE* arquivo2 = fopen("palavras.txt", "r");
     if(arquivo == NULL) {
         fprintf(stderr, "Erro ao abrir o arquivo.txt.");
         return 1;
     }
-    int i = 1;
-    char linha[1024];
+    int i = 1, tamanho = 0;
+    char linha[1024], linha2[1024];
+    char c, letra = '\n';
     
+    while(fgets(linha2, sizeof(linha2), arquivo2) != NULL) {
+        tamanho++;
+    }
+
     srand(time(NULL));
-    while(i <= rand() % 528) {
+    while(i <= rand() % tamanho) {
         fgets(linha, sizeof(linha), arquivo);
         i++;
     }
