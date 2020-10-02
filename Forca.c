547a399a38d524aa059279792eefa6a537ff57e1
Carla Ferreira <carlabeatrizferreira04@gmail.com>
2020-10-02 00:32:29 -0300

    "final"

--- Forca.c
+++ Forca.c
@@ -12,14 +12,13 @@ Jogo da forca em C com menu para escolher o tipo de jogo
 #include <time.h>
 #include <ctype.h>
 #define MAX 20
-#include "pessoa_pc.c"
 
 //#include <dos.h>
 
 //Declaração das funções:
 void menu();
 void multiplayer();
-//void pessoa_pc();
+void pessoa_pc();
 void AdicionarPalavras();
 
 //------
@@ -36,7 +35,7 @@ int main(){
   printf("\n| (3) Adicionar palavras ao jogo |");
   printf("\n| (4) Sair                       |");
   printf("\n----------------------------------");
-  printf("\n                by Carla Ferreira ");
+  printf("\n             by Carla and Victor | ");
 
   printf("\n\n->");
   scanf("%i", &opcao);
@@ -125,7 +124,90 @@ void multiplayer(){
 }
 
 //---------------
+void pessoa_pc(){  
+    FILE* fptr = fopen("palavras2.txt", "r");
+    FILE* fp = fopen("palavras2.txt", "r");
+    if(fptr == NULL) {
+        fprintf(stderr, "Erro ao abrir o palavras2.txt.");
+    }
+    int i = 1, linhas = 0, certo = 0, tamanho, tentativas = 10, chutes = 0;
+    char linha[50], linha2[50];
+    char c, letra[1], chutadas[50],  *preencher;
 
+    for(i = 0; i < 50; i++){
+        chutadas[i] = '1';
+    }
+
+    while(fgets(linha2, sizeof(linha2), fp) != NULL) {
+        linhas++;
+    }
+
+    srand(time(NULL));
+    while(i <= rand() % linhas) {
+        fgets(linha, sizeof(linha), fptr);
+        i++;
+    }
+
+    //------------------------------------------
+    
+    tamanho = strlen(linha);
+    tamanho --;
+    linha[tamanho] = ' ';
+    printf("\n tamanho: %d", tamanho);
+
+    for (i= 0; i < (tamanho); i++) linha[i] = tolower(linha[i]);
+
+    preencher = malloc(sizeof(char) * tamanho);
+
+    
+    for (i = 0; i < (tamanho); i++) preencher[i] = '-';
+    //for (i = 0; i < (tamanho); i++) printf("%c", preencher[i]);
+    
+    while (tentativas > 0 && certo != tamanho){
+        system("cls");
+        printf("\n");
+        for (i = 0; i < (tamanho); i++) printf("%c", preencher[i]);
+        
+        printf("\nDigite a letra que voce quer chutar: ");
+        fflush(stdin);
+        scanf("%c", letra);
+        letra[0] = tolower(letra[0]);
+
+        for(i = 0; i <= chutes; i++){
+            if(letra[0] == chutadas[i]){
+                while(letra[0] == chutadas[i]){
+                    printf("\nDigite a letra que voce quer chutar: ");
+                    fflush(stdin);
+                    scanf("%c", letra);
+                }
+            }
+        }
+        chutadas[chutes] = letra[0];
+        chutes++;
+        for (int i = 0; i < tamanho; i++){
+            if (linha[i] == letra[0])
+            {
+                preencher[i] = letra[0];
+                tentativas = tentativas +1;
+                certo ++;
+            }
+        }
+        if (certo == tamanho){
+            printf("\nPARABENS, VOCE VENCEU!!!");
+            printf("\nA palavra era \" %s\"", linha);
+            break;
+        }
+        tentativas = tentativas - 1;
+        printf("\nrestam %d tentativas", tentativas);
+    }
+     free(preencher);
+    /*
+    printf("\nTecle 1 para parar ou outro para continuar: ");
+    printf("\nO numero de vitorias foi: %d", vitorias);
+    printf("\nO numero de derrotas foi: %d", derrotas);*/
+    fclose(fptr);
+    fclose(fp);
+}
 //----------------
 void AdicionarPalavras(){
   char palavra [MAX];
