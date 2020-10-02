00e1e2125bc70343288b2aa255c7bdb5b02191cc
Carla Ferreira <carlabeatrizferreira04@gmail.com>
2020-10-01 23:29:32 -0300

    a

--- pessoa_pc.c
+++ pessoa_pc.c
@@ -23,56 +23,51 @@ void pessoa_pc(){
     printf("%s", linha);
 
     /*
-    while(continuar!=1){
-        x = rand() % tamarquivo;
-        fscanf ( fptr , " %s", palavras) ;
-        palavra = palavras[x];
-        printf("\n");
+    for (int i = 0; i < (tamanho); i++){
+    printf("%c", preencher[i]);
+    }
+    tamanho = strlen(palavra);
+    preencher = malloc(sizeof(char) * tamanho);
+    //printf("\nA palavra possui %i letras\n", tamanho);
+
+    for (int i=0; i<tamanho; i++);{
+    preencher[i] = '-' ;
+    }
+
+    //tirando o numero de espacos restantes
+    letra[0] = ' ';
+    for (int i = 0; i < (tamanho); i++) printf("%c", preencher[i]);
+    //posi = find (palavra==letra);
+    for (int i = 0; i < tamanho; i++){
+        if (palavra[i] == letra[0]){
+            preencher[i] = letra[0];
+        }
+    }
+    while (1){
+    //posi = find (palavra==letra);
+
+    //checando se a letra esta na palavra
+    printf("\nDigite a letra que voce quer chutar: ");
+    fflush(stdin);
+    scanf("%c", letra);
+    for (int i = 0; i < tamanho; i++){
+        if (palavra[i] == letra[0]){
+        preencher[i] = letra[0];
+        tentativas = tentativas +1;
+        certo ++;
+        }
+    }
+    //checando se acabou
+    if (certo == tamanho){
+        printf("\nPARABENS, VOCE VENCEU!!!");
+        printf("\nA palavra era \"");
         for (int i = 0; i < (tamanho); i++){
         printf("%c", preencher[i]);
         }
-        tamanho = strlen(palavra);
-        preencher = malloc(sizeof(char) * tamanho);
-        //printf("\nA palavra possui %i letras\n", tamanho);
-
-        for (int i=0; i<tamanho; i++);{
-        preencher[i] = '-' ;
-        }
-
-        //tirando o numero de espacos restantes
-        letra[0] = ' ';
-        for (int i = 0; i < (tamanho); i++) printf("%c", preencher[i]);
-        //posi = find (palavra==letra);
-        for (int i = 0; i < tamanho; i++){
-            if (palavra[i] == letra[0]){
-                preencher[i] = letra[0];
-            }
-        }
-        while (1){
-        //posi = find (palavra==letra);
-
-        //checando se a letra esta na palavra
-        printf("\nDigite a letra que voce quer chutar: ");
-        fflush(stdin);
-        scanf("%c", letra);
-        for (int i = 0; i < tamanho; i++){
-            if (palavra[i] == letra[0]){
-            preencher[i] = letra[0];
-            tentativas = tentativas +1;
-            certo ++;
-            }
-        }
-        //checando se acabou
-        if (certo == tamanho){
-            printf("\nPARABENS, VOCE VENCEU!!!");
-            printf("\nA palavra era \"");
-            for (int i = 0; i < (tamanho); i++){
-            printf("%c", preencher[i]);
-            }
-        printf("\"");
-        break;
-        }
-        }
+    printf("\"");
+    break;
+    }
+    }
     }
     printf("\nTecle 1 para parar ou outro para continuar: ");
     printf("\nO numero de vitorias foi: %d", vitorias);
